1e56a969781283e3a4763b1ea5adfb4a678a95ca
joy1954islam <joyislam1954@gmail.com>
2023-05-27 23:49:15 +0600

    update

--- email_sender/settings.py
+++ email_sender/settings.py
@@ -152,8 +152,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'joyislam1954job@gmail.com'
-EMAIL_HOST_PASSWORD = 'whgdaecewzadksxh'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 CORS_ORIGIN_WHITELIST = [
